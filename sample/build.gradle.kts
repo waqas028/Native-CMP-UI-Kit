ed9edbd3c059c37dfad28e2835406bc2fea48557
Muhammad Waqas <waqas028@users.noreply.github.com>
2026-03-30 16:28:53 +0500

    Fix Icons dependency and update testing instructions

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -56,7 +56,9 @@ kotlin {
             implementation(project(":native-cmp-kit"))
             implementation(compose.runtime)
             implementation(compose.foundation)
+            implementation(compose.material)
             implementation(compose.material3)
+            implementation(compose.materialIconsExtended)
             implementation(compose.ui)
             implementation(compose.components.resources)
         }
